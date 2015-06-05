552221eb1a0fa06b13ffc97541890ca05cc629ab
Mika <Mika>
2015-06-05 06:08:40 -0400

    IT LIVESSSSS
    * Python 2.7 cannot into Hexchat
    * Crash and burn
    * Added edge case for hooks
    * "Sanitize" callback input (strip colors, styles, anything that could
    go wrong hopefully)

--- trimmer.py
+++ trimmer.py
@@ -2,13 +2,15 @@ import hexchat
 
 __module_name__ = "trimmer"
 __module_author__ = "Mika Wu"
-__module_description__ = """Trims nicknames. Worst enemy of
-							aaaaaaaaaaaaaaaaaaaaaaa, DANtheBEASTman, et al."""
+__module_version__ = "0.1.0.150605"
+# Hexchat Python 2 API crashes calling any emit_print() functions.
+# Python 3 API does not, so we have to use it.
+__module_description__ = "Trims nicknames to length. REQUIRES PYTHON 3 PLUGIN."
 
 # Set to truncation level in characters.
 # Note that, if using a non-monospace font,
 # not all nicks will be of the same length.
-TRIM = 11
+TRIM = 10
 
 # Optionally remove one extra character from the end of the nickname and
 # replace it with the specified character to signify trimmed state.
@@ -33,20 +35,27 @@ def trim_cb(word, word_eol, userdata):
 
 	Returns:
 		EAT_* constant: Controls how Hexchat procedes when callback returns.
-		Here we will use "EAT_HEXCHAT" to soft "delete" the original message.
+		Here we will use "EAT_ALL" to soft "delete" the original message.
 
 	See HEXCHAT documentation for futher information.
 	"""
+	global TRIM, REPLACE_TRIM, SIGN
 
 	nick = word[0]
+	nick = hexchat.strip(nick, -1, 3)
 
 	if len(nick) > TRIM:
-		nick_trim = nick[0:TRIM - REPLACE_TRIM] + REPLACE_TIME * SIGN
+		nick_trim = nick[0:TRIM - REPLACE_TRIM] + REPLACE_TRIM * SIGN
+		# print("emit_print({}, {}, {})".format(userdata, nick_trim, word[1]))
 		hexchat.emit_print(userdata, nick_trim, word[1])
-		return hexchat.EAT_HEXCHAT
+		return hexchat.EAT_ALL
 
 
 # Script will check all messages and hilight messages with these two hooks.
 # Refer to Settings/Text Events for additional event names.
 hexchat.hook_print("Channel Message", trim_cb, "Channel Message")
 hexchat.hook_print("Channel Msg Hilight", trim_cb, "Channel Msg Hilight")
+# Hooks dialog windows w/ long nicks
+hexchat.hook_print("Private Message to Dialog", trim_cb, "Private Message to Dialog")
+#trim_cb(["attenuatedme","message", "@"], ["user","message", "@"], "Channel Message")
+print(__module_name__, __module_version__, "has been loaded.")
